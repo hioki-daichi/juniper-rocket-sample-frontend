dec816b754eb345d5d75da0e802d1c33b6b3cdcb
Daichi Hioki <hiokidaichi@gmail.com>
2019-11-30 23:19:37 +0900

    Enable to get and display videos

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,84 @@
+mod common;
+mod video;
+
+use common::helper::build_request;
+use common::response::ResponseData;
+use failure::Error;
+use serde_json::json;
+use video::model::Video;
+use video::response::VideosResponse;
+use yew::format::Json;
 use yew::prelude::*;
-use yew::services::ConsoleService;
+use yew::services::{fetch, ConsoleService, FetchService};
+use yew::virtual_dom::VNode;
 
 fn main() {
     yew::start_app::<Model>();
 }
 
 struct Model {
-    i: u8,
+    videos: Vec<Video>,
     console: ConsoleService,
+    link: ComponentLink<Model>,
+    fetch_service: FetchService,
+    fetch_task: Option<fetch::FetchTask>,
 }
 
 enum Msg {
-    Increment,
+    GetVideos,
+    GetVideosSuccess(Vec<Video>),
+    GetVideosFailure,
 }
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
 
-    fn create(_: Self::Properties, _component_link: ComponentLink<Self>) -> Self {
+    fn create(_: Self::Properties, component_link: ComponentLink<Self>) -> Self {
         Model {
-            i: 0,
+            videos: vec![],
             console: ConsoleService::new(),
+            link: component_link,
+            fetch_service: FetchService::new(),
+            fetch_task: None,
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        self.console.log(self.i.to_string().as_str());
-
         match msg {
-            Msg::Increment => self.i += 1,
+            Msg::GetVideos => {
+                self.console.log("Msg::GetVideos");
+
+                let data = json!({ "query": "{ videos { src } }" });
+                let request = build_request(&data);
+
+                let callback = self.link.send_back(
+                    move |response: fetch::Response<
+                        Json<Result<ResponseData<VideosResponse>, Error>>,
+                    >| {
+                        let (meta, Json(response_body)) = response.into_parts();
+                        if meta.status.is_success() {
+                            Msg::GetVideosSuccess(response_body.unwrap().data.videos)
+                        } else {
+                            Msg::GetVideosFailure
+                        }
+                    },
+                );
+
+                self.fetch_task = Some(self.fetch_service.fetch(request, callback));
+            }
+
+            Msg::GetVideosSuccess(videos) => {
+                self.console.log("Msg::GetVideosSuccess");
+
+                self.videos = videos;
+            }
+
+            Msg::GetVideosFailure => {
+                self.console.log("Msg::GetVideosFailure");
+
+                self.videos = vec![];
+            }
         }
 
         true
@@ -38,9 +87,19 @@ impl Component for Model {
     fn view(&self) -> Html<Self> {
         html! {
             <div class="container mx-auto">
-                <button onclick=|_| Msg::Increment>{ "Increment!" }</button>
-                {{ self.i }}
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onclick=|_| Msg::GetVideos>{ "Get Videos" }</button>
+                {{ self.videos.iter().map(|video| self.video_view(video)).collect::<VNode<Model>>() }}
             </div>
+        }
+    }
+}
+
+impl Model {
+    fn video_view(&self, video: &Video) -> Html<Self> {
+        html! {
+            <video class="w-full" autoplay=true muted=true loop=true playsinline=true>
+                <source src={video.src.as_str()} />
+            </video>
         }
     }
 }
