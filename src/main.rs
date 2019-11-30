e5bb6ec4659df5f04e2e01aab2ca43786981a989
Daichi Hioki <hiokidaichi@gmail.com>
2019-11-30 17:50:30 +0900

    Implement Counter App

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use yew::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<Model>();
+}
+
+struct Model {
+    i: u8,
+}
+
+enum Msg {
+    Increment,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, _component_link: ComponentLink<Self>) -> Self {
+        Model { i: 0 }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::Increment => self.i += 1,
+        }
+
+        true
+    }
+
+    fn view(&self) -> Html<Self> {
+        html! {
+            <div class="container mx-auto">
+                <button onclick=|_| Msg::Increment>{ "Increment!" }</button>
+                {{ self.i }}
+            </div>
+        }
+    }
 }
