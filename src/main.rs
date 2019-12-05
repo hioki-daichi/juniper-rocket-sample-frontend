0c7e8ba6261ad4dcbd02d197fe34ddc6babcbfc4
Daichi Hioki <hiokidaichi@gmail.com>
2019-12-05 17:37:37 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,35 @@ use common::helper::build_request;
 use common::response::ResponseData;
 use failure::Error;
 use serde_json::json;
-use video::model::Video;
+use video::model::{NewVideo, Video};
 use video::response::VideosResponse;
 use yew::format::Json;
 use yew::prelude::*;
+use yew::services::reader::{FileData, ReaderService, ReaderTask};
 use yew::services::{fetch, ConsoleService, FetchService};
+use yew::ChangeData;
 
 fn main() {
     yew::start_app::<Model>();
 }
 
 struct Model {
+    new_video: Option<NewVideo>,
     videos: Vec<Video>,
     console: ConsoleService,
     link: ComponentLink<Model>,
     fetch_service: FetchService,
     fetch_task: Option<fetch::FetchTask>,
+    reader_service: ReaderService,
+    reader_tasks: Vec<ReaderTask>,
 }
 
 enum Msg {
     GetVideos,
     GetVideosSuccess(Vec<Video>),
     GetVideosFailure,
+    ChooseFile(ChangeData),
+    LoadedFile(FileData),
 }
 
 impl Component for Model {
@@ -35,11 +42,14 @@ impl Component for Model {
 
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         Model {
+            new_video: None,
             videos: vec![],
             console: ConsoleService::new(),
             link,
             fetch_service: FetchService::new(),
             fetch_task: None,
+            reader_service: ReaderService::new(),
+            reader_tasks: vec![],
         }
     }
 
@@ -78,6 +88,27 @@ impl Component for Model {
 
                 self.videos = vec![];
             }
+
+            Msg::ChooseFile(ChangeData::Files(files)) => {
+                for file in files {
+                    self.reader_tasks.push(
+                        self.reader_service
+                            .read_file(file, self.link.send_back(move |v| Msg::LoadedFile(v))),
+                    );
+                }
+            }
+
+            Msg::ChooseFile(ChangeData::Value(_)) => {
+                self.console.log("ChangeData::Value");
+            }
+
+            Msg::ChooseFile(ChangeData::Select(_)) => {
+                self.console.log("ChangeData::Select");
+            }
+
+            Msg::LoadedFile(file_data) => {
+                self.new_video = Some(NewVideo::build(file_data));
+            }
         }
 
         true
@@ -87,6 +118,8 @@ impl Component for Model {
         html! {
             <div class="container mx-auto">
                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onclick=|_| Msg::GetVideos>{ "Get Videos" }</button>
+                <input type="file" onchange = |change_data| Msg::ChooseFile(change_data) />
+                {{ self.view_new_video(self.new_video.clone()) }}
                 { for self.videos.iter().map(|video| self.video_view(video)) }
             </div>
         }
@@ -99,6 +132,18 @@ impl Model {
             <video class="w-full" autoplay=true muted=true loop=true playsinline=true>
                 <source src={video.src.as_str()} />
             </video>
+        }
+    }
+
+    fn view_new_video(&self, new_video: Option<NewVideo>) -> Html<Self> {
+        let a = new_video.unwrap_or(NewVideo {
+            src: "".to_string(),
+        });
+
+        html! {
+          <video class="w-full" autoplay=true muted=true loop=true playsinline=true>
+              <source src={a.src.as_str()} />
+          </video>
         }
     }
 }
--- src/video/model.rs
+++ src/video/model.rs
@@ -1,6 +1,23 @@
 use serde::*;
+use yew::services::reader::FileData;
 
 #[derive(Deserialize)]
 pub struct Video {
     pub src: String,
+}
+
+#[derive(Clone)]
+pub struct NewVideo {
+    pub src: String,
+}
+
+impl NewVideo {
+    pub fn build(file_data: FileData) -> Self {
+        let src = format!(
+            "data:image/gif;base64,{}",
+            base64::encode(&file_data.content)
+        );
+
+        Self { src }
+    }
 }
