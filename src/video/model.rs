295ba0bf2e69d2dcfa9e0bf10c16b1db7f5d3325
Daichi Hioki <hiokidaichi@gmail.com>
2019-12-09 23:00:50 +0900

    Add method: Video::new

--- src/video/model.rs
+++ src/video/model.rs
@@ -1,6 +1,18 @@
 use serde::*;
+use yew::services::reader::FileData;
 
 #[derive(Deserialize, Debug)]
 pub struct Video {
     pub src: String,
+}
+
+impl Video {
+    pub fn new(file_data: FileData) -> Self {
+        let src = format!(
+            "data:video/mp4;base64,{}",
+            base64::encode(&file_data.content)
+        );
+
+        Self { src }
+    }
 }
